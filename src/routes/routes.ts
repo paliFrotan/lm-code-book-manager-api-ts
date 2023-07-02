f61dc85082541c5f6236967e11894b766f010c2d
Pali Frotan <pali.frotan@icloud.com>
2023-07-02 22:04:54 +0100

    Feat: Implement test for delete

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,3 +8,5 @@ router.post("/books", booksController.saveBook);
 
 // User Story 4 - Update Book By Id Solution
 router.put("/books/:bookId", booksController.updateBook);
+// User Story 5 - Delete book by Id
+router.delete("/books/:bookId", booksController.deleteBook);
